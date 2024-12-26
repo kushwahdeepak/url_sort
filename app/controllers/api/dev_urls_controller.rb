348dd36fb9c79ac3839eec47a137389a125a940e
deepak <deepakkushwahsk@gmail.com>
2024-12-26 15:12:58 +0530

    verify token and create sort after verify auth token

--- app/controllers/api/dev_urls_controller.rb
+++ app/controllers/api/dev_urls_controller.rb
@@ -1,11 +1,23 @@
 class Api::DevUrlsController < ApplicationController
-    
+    skip_before_action :verify_authenticity_token  # Skip CSRF protection for API
+
+    before_action :authenticate
+
+    API_TOKEN = ENV['API_TOKEN'] || "c916f711343c8ec98e0d210fb41f6e6b46"
     def create
-        @url = DevUrl.new(original: params[:original_url])
+        @url = DevUrl.new(original_url: params[:original_url])
         if @url.save
           render json: { short_url: short_url(@url.short_url) }, status: :created
         else
           render json: { error: @url.errors.full_messages }, status: :unprocessable_entity
         end
-      end
+    end
+
+      private
+
+    def authenticate
+      token = request.headers['Authorization']
+      head :unauthorized unless token == API_TOKEN
+    end
+
 end
